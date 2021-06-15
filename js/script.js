55e2b9f04165c9338a3115380f06e1263717fb52
Kaleb-Moore <k4l3bmoore@gmail.com>
2021-06-15 10:07:15 -0600

    Update script.js
    Input property for objects and populated most properties for testing

--- js/script.js
+++ js/script.js
@@ -12,21 +12,96 @@ project 1 - A Random Quote Generator
 ***/
 
 let quotes = [
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
+  {
+    quote: 'If you always do what you’ve always done, you always get what you’ve always gotten.',
+    source: 'Jessie Potter',
+    citation: 'The Milwaukee Sentinel',
+    year: '1981',
+  },
+  {
+    quote: 'I have no special talent. I am only passionately curious.',
+    source: 'Albert Einstein',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'All that we are is the result of what we have thought. The mind is everything. What we think we become.',
+    source: 'Buddha',
+    citation: '',
+    year: '625 B.C.',
+  },
+  {
+    quote: 'Stay hungry, stay foolish.',
+    source: 'Stewart Brand',
+    citation: 'The Whole Earth Catalog - Final Issue',
+    year: '1974',
+  },
+  {
+    quote: 'Insanity: doing the same thing over and over again and expecting different results.',
+    source: 'Albert Einstein',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Don’t count the days, make the days count.',
+    source: 'Muhammad Ali',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Well done is better than well said.',
+    source: 'Benjamin Franklin',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'I came, I saw, I conquered.',
+    source: 'Julius Caesar',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Be yourself; everyone else is already taken.',
+    source: 'Oscar Wilde',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'The only thing that interferes with my learning is my education.',
+    source: 'Albert Einstein',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Education is the most powerful weapon which you can use to change the world.',
+    source: 'Nelson Mandela',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Little by little, one travels far.',
+    source: 'J.R.R Tolkien',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'It’s not how much you have that makes people look up to you, it’s who you are.',
+    source: 'Elvis Presley',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.',
+    source: 'Bruce Lee',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Love all, trust a few, do wrong to none.',
+    source: 'William Shakespeare',
+    citation: '',
+    year: '',
+  },
   ];
 
 /***
